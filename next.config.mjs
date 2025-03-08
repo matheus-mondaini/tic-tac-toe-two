e2a823fc84d88cdd3592d29a9acdf2c0db473acd
Matheus Mondaini Alegre de Miranda <matheus.mondaini@outlook.com>
2025-03-08 20:41:33 -0300

    fix: try to fix gh pages css by changing next config, but it still works at localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/tic-tac-toe-two' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/tic-tac-toe-two/' : '',
-  transpilePackages: ['next'],
-  trailingSlash: true,
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/tic-tac-toe-two' : ''
+};
 
 export default nextConfig
